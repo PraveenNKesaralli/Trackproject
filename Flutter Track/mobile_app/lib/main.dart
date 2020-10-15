2f13410b0823b77e472b9c75eaa5a7b06751d5d0
Praveen Nagaraj <praveennagaraj70@gmail.com>
2020-10-15 16:09:57 +0530

    flutter commit

--- Flutter Track/mobile_app/lib/main.dart
+++ Flutter Track/mobile_app/lib/main.dart
@@ -8,44 +8,107 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-        body: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
-          Text(
-            'TRADENAPP',
-            style: TextStyle(fontSize: 40, color: Colors.lightBlueAccent),
-          ),
-          Image.asset('asstes/welcome.png'),
-          new Container(
-            margin: const EdgeInsets.all(20),
-            child: SizedBox(
-              width: 800,
-              height: 50,
-              child: RaisedButton(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(18.0),
+      title: 'Tradenapp Homepage',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MyHomePage(title: 'Tradenapp Homepage'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  MyHomePage({Key key, this.title}) : super(key: key);
+
+  final String title;
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  TextStyle style = TextStyle(fontFamily: 'Montserrat', fontSize: 20.0);
+
+  @override
+  Widget build(BuildContext context) {
+    final loginButon = Material(
+      elevation: 10.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Colors.lightBlue[700],
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () {},
+        child: Text("Login",
+            textAlign: TextAlign.center,
+            style: style.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold)),
+      ),
+    );
+
+    final signupButon = Material(
+      elevation: 10.0,
+      borderRadius: BorderRadius.circular(30.0),
+      color: Colors.lightBlue[700],
+      child: MaterialButton(
+        minWidth: MediaQuery.of(context).size.width,
+        padding: EdgeInsets.fromLTRB(20.0, 15.0, 20.0, 15.0),
+        onPressed: () {},
+        child: Text("Sign Up",
+            textAlign: TextAlign.center,
+            style: style.copyWith(
+                color: Colors.white, fontWeight: FontWeight.bold)),
+      ),
+    );
+    return Scaffold(
+      body: Center(
+        child: Container(
+          color: Colors.white,
+          child: Padding(
+            padding: const EdgeInsets.all(40.0),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.center,
+              mainAxisAlignment: MainAxisAlignment.center,
+              children: <Widget>[
+                Text(
+                  'TRADENAPP',
+                  textAlign: TextAlign.center,
+                  style: style.copyWith(
+                      color: Colors.lightBlue[700],
+                      fontWeight: FontWeight.bold,
+                      fontSize: 35.0),
                 ),
-                onPressed: () {},
-                child: Text('Login'),
-                color: Colors.blue,
-              ),
+                SizedBox(
+                    height: 400.0,
+                    child: ListView(children: [
+                      Image.asset(
+                        "assets/welcome.png",
+                        fit: BoxFit.contain,
+                      ),
+                      Text(
+                        'Find services near you',
+                        textAlign: TextAlign.center,
+                        style: style.copyWith(
+                            color: Colors.lightBlue[700],
+                            fontWeight: FontWeight.bold),
+                      ),
+                      SizedBox(height: 10),
+                      Text(
+                        'Search for local and international services and browse between them',
+                        textAlign: TextAlign.center,
+                      ),
+                    ])),
+                loginButon,
+                SizedBox(
+                  height: 15.0,
+                ),
+                signupButon,
+                SizedBox(
+                  height: 15.0,
+                ),
+              ],
             ),
           ),
-          Container(
-            margin: const EdgeInsets.all(20),
-            child: SizedBox(
-              width: 800,
-              height: 50,
-              child: RaisedButton(
-                shape: RoundedRectangleBorder(
-                  borderRadius: BorderRadius.circular(18.0),
-                ),
-                onPressed: () {},
-                child: Text('Sign up'),
-                color: Colors.blue,
-              ),
-            ),
-          ),
-        ]),
+        ),
       ),
     );
   }
