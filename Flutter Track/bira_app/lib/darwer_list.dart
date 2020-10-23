79b9a07465a638ca29e2dc9e8f49e72e25a71900
Praveen Nagaraj <praveennagaraj70@gmail.com>
2020-10-23 19:35:40 +0530

    flutter commit

---
+++ Flutter Track/bira_app/lib/darwer_list.dart
@@ -0,0 +1,91 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_svg/flutter_svg.dart';
+import 'package:bira_app/main.dart';
+
+class DrawerPage extends StatelessWidget {
+  final bool isSelected = false;
+  @override
+  Widget build(BuildContext context) {
+    return Drawer(
+      child: Container(
+        color: Colors.purple[900],
+        child: ListView(
+          padding: EdgeInsets.zero,
+          children: [
+            SizedBox(
+              height: 40,
+            ),
+            ListTile(
+              leading: SvgPicture.asset(
+                'assets/close.svg',
+                height: 20,
+                width: 25,
+              ),
+              onTap: () {
+                Navigator.of(context).pop();
+              },
+              title: Text(
+                'Menu',
+                style: TextStyle(color: Colors.white),
+              ),
+            ),
+            Ink(
+              color: isSelected ? Colors.blue : Colors.transparent,
+              child: ListTile(
+                leading: SvgPicture.asset(
+                  'assets/Dashboard.svg',
+                  height: 20,
+                  width: 25,
+                  color: Colors.purple,
+                ),
+                title: Text(
+                  'Dashboard',
+                  style: TextStyle(color: Colors.purple),
+                ),
+                onTap: () {},
+              ),
+            ),
+            Ink(
+              color: isSelected ? Colors.blue : Colors.transparent,
+              child: ListTile(
+                leading: SvgPicture.asset(
+                  'assets/Issues.svg',
+                  height: 20,
+                  width: 25,
+                  color: Colors.purple,
+                ),
+                title: Text(
+                  'Issues',
+                  style: TextStyle(color: Colors.purple),
+                ),
+                onTap: () {
+                  Navigator.of(context).pop();
+                  Navigator.push(context,
+                      MaterialPageRoute(builder: (context) => MyHomePage()));
+                },
+              ),
+            ),
+            Ink(
+              color: isSelected ? Colors.blue : Colors.transparent,
+              child: ListTile(
+                leading: SvgPicture.asset(
+                  'assets/Create.svg',
+                  height: 20,
+                  width: 25,
+                  color: Colors.purple,
+                ),
+                title: Text(
+                  'Create',
+                  style: TextStyle(
+                    color: Colors.purple,
+                  ),
+                ),
+                onTap: () {},
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
--- Flutter Track/bira_app/lib/main.dart
+++ Flutter Track/bira_app/lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:bira_app/darwer_list.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/flutter_svg.dart';
 
@@ -43,20 +44,17 @@ class _MyHomePageState extends State<MyHomePage> {
             child: InkWell(
               onTap: () {},
               child: Column(
-                // mainAxisAlignment: MainAxisAlignment.center,
-                //crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   Container(
-                    padding:
-                        const EdgeInsets.only(top: 20, left: 20, right: 20),
+                    padding: const EdgeInsets.fromLTRB(20, 20, 20, 10),
                     child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
                       children: [
                         Text(
                           'ID:PCG836342',
                           style: TextStyle(
                               fontSize: 10, fontWeight: FontWeight.w600),
                         ),
-                        Spacer(),
                         Text(
                           'January 02,2019',
                           style: TextStyle(color: Colors.grey, fontSize: 12),
@@ -67,7 +65,6 @@ class _MyHomePageState extends State<MyHomePage> {
                   Padding(
                     padding: const EdgeInsets.fromLTRB(20, 15, 20, 0),
                     child: Column(
-                      //  mainAxisAlignment: MainAxisAlignment.center,
                       crossAxisAlignment: CrossAxisAlignment.start,
                       children: [
                         Text(
@@ -96,27 +93,169 @@ class _MyHomePageState extends State<MyHomePage> {
                       children: [Text('Assignee'), Spacer(), Text('Status')],
                     ),
                   ),
-                  // Container(
-                  //   child: Row(
-                  //     children: [
-                  //       Container(
-                  //         child: Row(
-                  //           children: [
-                  //             //  Image.asset('assets/sigo.svg'),
-                  //             ListTile(
-                  //               title: Text('Sijo M Peter'),
-                  //               subtitle: Text('UI/UX Designer'),
-                  //             )
-                  //           ],
-                  //         ),
-                  //       )
-                  //     ],
-                  //   ),
-                  // )
+                  Padding(
+                    padding: EdgeInsets.only(left: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        SvgPicture.asset(
+                          'assets/Sijo.svg',
+                        ),
+                        Padding(
+                            padding: EdgeInsets.only(left: 5),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Sijo M Peter',
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                Text(
+                                  'UI/UX Designer',
+                                  style: TextStyle(
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.w400),
+                                ),
+                              ],
+                            )),
+                        Spacer(),
+                        SizedBox(
+                          height: 34.0,
+                          child: Card(
+                            color: Colors.red,
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+                              child: Text(
+                                'High Priority',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 12),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        ),
+      ));
+    }
 
-                  SvgPicture.asset(
-                    'assets/Sijo.svg',
-                  )
+    Widget buildcard2() {
+      return (SizedBox(
+        height: 250.0,
+        width: 380.0,
+        child: Padding(
+          padding: const EdgeInsets.only(bottom: 10),
+          child: Card(
+            clipBehavior: Clip.antiAlias,
+            elevation: 5.0,
+            child: InkWell(
+              onTap: () {},
+              child: Column(
+                children: [
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(20, 20, 20, 10),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                      children: [
+                        Text(
+                          'ID:PCG836342',
+                          style: TextStyle(
+                              fontSize: 10, fontWeight: FontWeight.w600),
+                        ),
+                        Text(
+                          'January 09,2019',
+                          style: TextStyle(color: Colors.grey, fontSize: 12),
+                        )
+                      ],
+                    ),
+                  ),
+                  Padding(
+                    padding: const EdgeInsets.fromLTRB(20, 15, 20, 0),
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Text(
+                          'Lorem ipsum dolor sit',
+                          style: TextStyle(
+                            fontSize: 18,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 5,
+                        ),
+                        Text(
+                          'Lorem ipsumn dolor sit amet, conctetur adipiscing elit,Ut ut gravida dolor, Phaselus vitae sim',
+                          style: TextStyle(
+                              fontSize: 12,
+                              fontWeight: FontWeight.w400,
+                              fontStyle: FontStyle.normal),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    padding: const EdgeInsets.fromLTRB(20, 20, 20, 5),
+                    child: Row(
+                      children: [Text('Assignee'), Spacer(), Text('Status')],
+                    ),
+                  ),
+                  Padding(
+                    padding: EdgeInsets.only(left: 20),
+                    child: Row(
+                      mainAxisAlignment: MainAxisAlignment.start,
+                      crossAxisAlignment: CrossAxisAlignment.end,
+                      children: [
+                        SvgPicture.asset(
+                          'assets/Sijo.svg',
+                        ),
+                        Padding(
+                            padding: EdgeInsets.only(left: 5),
+                            child: Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                Text(
+                                  'Sijo M Peter',
+                                  style: TextStyle(
+                                      fontSize: 16,
+                                      fontWeight: FontWeight.w600),
+                                ),
+                                Text(
+                                  'UI/UX Designer',
+                                  style: TextStyle(
+                                      fontSize: 10,
+                                      fontWeight: FontWeight.w400),
+                                ),
+                              ],
+                            )),
+                        Spacer(),
+                        SizedBox(
+                          height: 34.0,
+                          child: Card(
+                            color: Colors.green,
+                            child: Padding(
+                              padding: const EdgeInsets.fromLTRB(10, 5, 10, 5),
+                              child: Text(
+                                'Low Priority',
+                                style: TextStyle(
+                                    color: Colors.white, fontSize: 12),
+                              ),
+                            ),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
                 ],
               ),
             ),
@@ -126,22 +265,45 @@ class _MyHomePageState extends State<MyHomePage> {
     }
 
     return Scaffold(
-      backgroundColor: Colors.blueGrey[50],
+      drawer: DrawerPage(),
+      backgroundColor: Colors.cyan[50],
       appBar: AppBar(
-        backgroundColor: Colors.blue,
-        // leading: Image.asset('assets/Logo.svg'),
+        backgroundColor: Colors.white,
+        leading: Builder(
+            builder: (context) => IconButton(
+                icon: new Icon(
+                  Icons.menu,
+                  color: Colors.blue,
+                ),
+                onPressed: () => Scaffold.of(context).openDrawer())),
         title: Text(
           'tracker',
-          style: TextStyle(fontWeight: FontWeight.bold, color: Colors.black),
+          style: TextStyle(
+            fontWeight: FontWeight.bold,
+            color: Colors.black,
+          ),
         ),
         actions: [
           IconButton(
-              icon: Icon(Icons.notifications_on_outlined), onPressed: () {}),
-          IconButton(icon: Icon(Icons.search), onPressed: () {}),
-          IconButton(icon: Icon(Icons.more_vert), onPressed: () {}),
+              icon: Icon(
+                Icons.notifications_on_outlined,
+                color: Colors.grey,
+              ),
+              onPressed: () {}),
+          IconButton(
+              icon: Icon(
+                Icons.search,
+                color: Colors.grey,
+              ),
+              onPressed: () {}),
+          IconButton(
+              icon: Icon(
+                Icons.more_vert,
+                color: Colors.grey,
+              ),
+              onPressed: () {}),
         ],
       ),
-      drawer: Drawer(),
       body: Center(
         child: ListView(
           children: <Widget>[
@@ -157,16 +319,29 @@ class _MyHomePageState extends State<MyHomePage> {
                           TextStyle(fontWeight: FontWeight.w600, fontSize: 30),
                     ),
                   ),
+                  Spacer(),
+                  Text(
+                    'Filter',
+                    style: TextStyle(color: Colors.grey, fontSize: 18),
+                  ),
+                  SizedBox(
+                    width: 5,
+                  ),
+                  Text(
+                    'Project',
+                    style: TextStyle(fontWeight: FontWeight.w600, fontSize: 18),
+                  ),
+                  Icon(Icons.arrow_drop_down)
                 ],
               ),
             ),
             SizedBox(
-              height: 1000,
+              height: 1330,
               width: 500,
               child: Container(
-                padding: const EdgeInsets.all(15),
+                padding: const EdgeInsets.only(left: 15, right: 15),
                 child: Card(
-                  color: Colors.pink[50],
+                  color: Colors.cyan[200],
                   elevation: 10,
                   child: Column(
                     children: <Widget>[
@@ -180,16 +355,72 @@ class _MyHomePageState extends State<MyHomePage> {
                                   fontWeight: FontWeight.bold, fontSize: 20),
                             ),
                             Spacer(),
-                            Icon(Icons.arrow_drop_down_outlined),
+                            Icon(Icons.arrow_drop_down_sharp),
                           ],
                         ),
                       ),
                       buildcard(),
+                      buildcard(),
+                      buildcard2(),
+                      buildcard(),
+                      buildcard2(),
                     ],
                   ),
                 ),
               ),
-            )
+            ),
+            SizedBox(
+              height: 60,
+              child: Container(
+                padding: const EdgeInsets.only(left: 15, right: 15),
+                child: Card(
+                    child: Padding(
+                  padding: EdgeInsets.only(
+                    right: 20,
+                    left: 20,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'In Progress 2',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w600),
+                      ),
+                      IconButton(
+                          icon: Icon(Icons.arrow_forward_ios_outlined),
+                          onPressed: () {})
+                    ],
+                  ),
+                )),
+              ),
+            ),
+            SizedBox(
+              height: 60,
+              child: Container(
+                padding: const EdgeInsets.only(left: 15, right: 15),
+                child: Card(
+                    child: Padding(
+                  padding: EdgeInsets.only(
+                    right: 20,
+                    left: 20,
+                  ),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Text(
+                        'Done 5',
+                        style: TextStyle(
+                            fontSize: 20, fontWeight: FontWeight.w600),
+                      ),
+                      IconButton(
+                          icon: Icon(Icons.arrow_forward_ios_outlined),
+                          onPressed: () {})
+                    ],
+                  ),
+                )),
+              ),
+            ),
           ],
         ),
       ),
